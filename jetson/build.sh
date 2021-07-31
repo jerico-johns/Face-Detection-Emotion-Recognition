9128fb1310695b1372d7cba4e5458ee073facbaa
sudhrity <sudhrity@gmail.com>
2021-07-30 19:26:02 -0700

    update jetson docker

--- jetson/build.sh
+++ jetson/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t mids-w251-project:2 .
+docker build -t mids-w251-project-nx:1 .
--- jetson/run.sh
+++ jetson/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export DISPLAY=:0
 sudo xhost +
-sudo docker run --gpus all --privileged  -it --rm --device /dev/video0 -e DISPLAY=$DISPLAY --runtime nvidia --network host -v ~/mids-w251-project:/mids-w251-project mids-w251-project:2 bash
+sudo docker run --gpus all --privileged  -it --shm-size=2048m --rm --device /dev/video0 -e DISPLAY=$DISPLAY --runtime nvidia --network host -v ~/mids-w251-project:/mids-w251-project mids-w251-project-nx:1 bash
