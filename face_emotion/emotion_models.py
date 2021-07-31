a1d98df6f60224ed5a939c3844987499d8af5604
sudhrity <sudhrity@gmail.com>
2021-07-30 19:11:51 -0700

    cleaning up dirs and files

---
+++ face_emotion/emotion_models.py
@@ -0,0 +1,222 @@
+import torch.nn as nn
+import torch
+
+def get_default_device():
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+    
+def to_device(data, device):
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        return len(self.dl)
+
+device = get_default_device()
+
+class Face_Emotion_CNN(nn.Module):
+  def __init__(self):
+    super(Face_Emotion_CNN, self).__init__()
+    self.cnn1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3)
+    self.cnn2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3)
+    self.cnn3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
+    self.cnn4 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
+    self.cnn5 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3)
+    self.cnn6 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3)
+    self.cnn7 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3)
+    self.relu = nn.ReLU()
+    self.pool1 = nn.MaxPool2d(2, 1)
+    self.pool2 = nn.MaxPool2d(2, 2)
+    self.cnn1_bn = nn.BatchNorm2d(8)
+    self.cnn2_bn = nn.BatchNorm2d(16)
+    self.cnn3_bn = nn.BatchNorm2d(32)
+    self.cnn4_bn = nn.BatchNorm2d(64)
+    self.cnn5_bn = nn.BatchNorm2d(128)
+    self.cnn6_bn = nn.BatchNorm2d(256)
+    self.cnn7_bn = nn.BatchNorm2d(256)
+    self.fc1 = nn.Linear(1024, 512)
+    self.fc2 = nn.Linear(512, 256)
+    self.fc3 = nn.Linear(256, 7)
+    self.dropout = nn.Dropout(0.3)
+    self.log_softmax = nn.LogSoftmax(dim=1)
+    
+  def forward(self, x):
+    x = self.relu(self.pool1(self.cnn1_bn(self.cnn1(x))))
+    x = self.relu(self.pool1(self.cnn2_bn(self.dropout(self.cnn2(x)))))
+    x = self.relu(self.pool1(self.cnn3_bn(self.cnn3(x))))
+    x = self.relu(self.pool1(self.cnn4_bn(self.dropout(self.cnn4(x)))))
+    x = self.relu(self.pool2(self.cnn5_bn(self.cnn5(x))))
+    x = self.relu(self.pool2(self.cnn6_bn(self.dropout(self.cnn6(x)))))
+    x = self.relu(self.pool2(self.cnn7_bn(self.dropout(self.cnn7(x)))))
+    
+    x = x.view(x.size(0), -1)
+    
+    x = self.relu(self.dropout(self.fc1(x)))
+    x = self.relu(self.dropout(self.fc2(x)))
+    x = self.log_softmax(self.fc3(x))
+    return x
+
+  def count_parameters(self):
+    return sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)
+        loss = F.cross_entropy(out, labels)
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)
+        loss = F.cross_entropy(out, labels)
+        acc = accuracy(out, labels)
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))
+        
+class ResNet_m2(ImageClassificationBase):
+  def __init__(self):
+    super(ResNet, self).__init__()
+    self.cnn1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3)
+    self.cnn2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3)
+    self.cnn3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
+    self.cnn4 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
+    self.cnn5 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3)
+    self.cnn6 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3)
+    self.cnn7 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3)
+    self.relu = nn.ReLU()
+    self.pool1 = nn.MaxPool2d(2, 1)
+    self.pool2 = nn.MaxPool2d(2, 2)
+    self.cnn1_bn = nn.BatchNorm2d(8)
+    self.cnn2_bn = nn.BatchNorm2d(16)
+    self.cnn3_bn = nn.BatchNorm2d(32)
+    self.cnn4_bn = nn.BatchNorm2d(64)
+    self.cnn5_bn = nn.BatchNorm2d(128)
+    self.cnn6_bn = nn.BatchNorm2d(256)
+    self.cnn7_bn = nn.BatchNorm2d(256)
+    self.fc1 = nn.Linear(1024, 512)
+    self.fc2 = nn.Linear(512, 256)
+    self.fc3 = nn.Linear(256, 7)
+    self.dropout = nn.Dropout(0.3)
+    self.log_softmax = nn.LogSoftmax(dim=1)
+
+  def forward(self, x):
+    x = self.relu(self.pool1(self.cnn1_bn(self.cnn1(x))))
+    x = self.relu(self.pool1(self.cnn2_bn(self.dropout(self.cnn2(x)))))
+    x = self.relu(self.pool1(self.cnn3_bn(self.cnn3(x))))
+    x = self.relu(self.pool1(self.cnn4_bn(self.dropout(self.cnn4(x)))))
+    x = self.relu(self.pool2(self.cnn5_bn(self.cnn5(x))))
+    x = self.relu(self.pool2(self.cnn6_bn(self.dropout(self.cnn6(x)))))
+    x = self.relu(self.pool2(self.cnn7_bn(self.dropout(self.cnn7(x)))))
+
+    x = x.view(x.size(0), -1)
+
+    x = self.relu(self.dropout(self.fc1(x)))
+    x = self.relu(self.dropout(self.fc2(x)))
+    x = self.log_softmax(self.fc3(x))
+    return x
+
+  def count_parameters(self):
+    return sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+  def training_step(self, batch):
+    images, labels = batch 
+    out = self(images)
+    loss = F.cross_entropy(out, labels)
+    return loss
+    
+  def validation_step(self, batch):
+    images, labels = batch 
+    out = self(images)
+    loss = F.cross_entropy(out, labels)
+    acc = accuracy(out, labels)
+    return {'val_loss': loss.detach(), 'val_acc': acc}
+
+  def validation_epoch_end(self, outputs):
+    batch_losses = [x['val_loss'] for x in outputs]
+    epoch_loss = torch.stack(batch_losses).mean()
+    batch_accs = [x['val_acc'] for x in outputs]
+    epoch_acc = torch.stack(batch_accs).mean()
+    return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+  def epoch_end(self, epoch, result):
+    print("Epoch [{}], last_lr: {:.6f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+    epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))    
+    
+
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
+              nn.BatchNorm2d(out_channels), 
+              nn.ELU(inplace=True)]
+    if pool: layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+class ResNet(ImageClassificationBase):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        
+        self.conv1 = conv_block(in_channels, 128)
+        self.conv2 = conv_block(128, 128, pool=True)
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
+        self.drop1 = nn.Dropout(0.5)
+        
+        self.conv3 = conv_block(128, 256)
+        self.conv4 = conv_block(256, 256, pool=True)
+        self.res2 = nn.Sequential(conv_block(256, 256), conv_block(256, 256))
+        self.drop2 = nn.Dropout(0.5)
+        
+        self.conv5 = conv_block(256, 512)
+        self.conv6 = conv_block(512, 512, pool=True)
+        self.res3 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+        self.drop3 = nn.Dropout(0.5)
+        
+        self.classifier = nn.Sequential(nn.MaxPool2d(6), 
+                                        nn.Flatten(),
+                                        nn.Linear(512, num_classes))
+        
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.drop1(out)
+        
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.drop2(out)
+        
+        out = self.conv5(out)
+        out = self.conv6(out)
+        out = self.res3(out) + out
+        out = self.drop3(out)
+        
+        out = self.classifier(out)
+        return out
+
+if __name__ == '__main__':
+    bn_model = Face_Emotion_CNN()
+    x = torch.randn(1,1,48,48)
+    print('Shape of output = ',bn_model(x).shape)
+    print('No of Parameters of the BatchNorm-CNN Model =',bn_model.count_parameters())
--- jetson/run.sh
+++ jetson/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+export DISPLAY=:0
+sudo xhost +
 sudo docker run --gpus all --privileged  -it --rm --device /dev/video0 -e DISPLAY=$DISPLAY --runtime nvidia --network host -v ~/mids-w251-project:/mids-w251-project mids-w251-project:2 bash
